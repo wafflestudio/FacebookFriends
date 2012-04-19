65af4369a5fb5a0f927d99798bd79b7e647bd69c
tantara <tantara@waffledev.(none)>
2012-04-20 00:41:03 +0900

    first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookFriend::Application.config.secret_token = '6dbb1d2346f9f9e17a1e89712c2c637e695804e5b57e947643cb75f8032809f0058de06c824f1a3b1752ab826dec7ab32dbdb3ea362050a6e302bd48cdee8ac5'
